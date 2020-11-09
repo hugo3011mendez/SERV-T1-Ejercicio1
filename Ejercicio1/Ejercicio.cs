b76b02d5b7c203ec7b0d96b5ddc86d1c55b9a64d
Hugo Mendez <hugo3011mendez@gmail.com>
2020-11-09 20:32:33 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Ejercicio.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Ejercicio
+    {
+        public delegate void MyDelegate();
+
+        static void MenuGenerator(String[] nombreOpciones, MyDelegate[] opciones)
+        {
+            bool repetir = true;
+            int opcion;
+
+            while (repetir)
+            {
+                // Muestro las opciones que haya en el array de sus nombres
+                for (int i = 0; i < nombreOpciones.Length; i++)
+                {
+                    Console.WriteLine(i + 1 + " : " + nombreOpciones[i]);
+                }
+
+                Console.WriteLine((nombreOpciones.Length + 1) + " : Salir");
+                Console.WriteLine();
+
+                // Le pido al usuario que escoja una opción
+                Console.WriteLine("Qué opción escoges?");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                if (opcion > 0 && opcion <= nombreOpciones.Length + 1)
+                {
+                    if (opcion == nombreOpciones.Length + 1)
+                    {
+                        repetir = false;
+                        Console.WriteLine("Pulsa Enter para salir");
+                    }
+                    else
+                    {
+                        MyDelegate caso = new MyDelegate(opciones[opcion-1]);
+                        caso();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No existe esa opción!");
+                }
+            }
+        }
+
+
+        static void f1()
+        {
+            Console.WriteLine("A");
+        }
+        static void f2()
+        {
+            Console.WriteLine("B");
+        }
+        static void f3()
+        {
+            Console.WriteLine("C");
+        }
+
+
+        static void Main(string[] args)
+        {
+            MenuGenerator(new string[] { "Op1", "Op2", "Op3" },
+            new MyDelegate[] { f1, f2, f3 });
+            Console.ReadKey();
+        }
+    }
+}
